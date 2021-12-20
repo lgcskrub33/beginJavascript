083d7952d79bc044e2b11cbc1ee199e7f95bc000
csk <lgcskrub33@gmail.com>
2021-12-20 13:50:24 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const calculator = {
+    plus : function(a,b){
+        return a+b;
+    },
+}
+const test="test";
+const test2="test2";
+const test3="test3";
+// const plusResult = calculator.plus(2,3);
+
+plusResult = calculator.plus(4,3);
